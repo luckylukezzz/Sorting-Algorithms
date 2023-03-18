4ab9bca27966b362cf37f0b91b9a30ec4b225637
luckylukezzz <patalee2001@gmail.com>
2023-03-19 00:00:03 +0530

    v1

--- Merge sort/mergesort.cpp
+++ Merge sort/mergesort.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void merge(int x[],int y[],int arr[]){
-    int lenx=sizeof(x)/sizeof(int);
-    int leny=sizeof(y)/sizeof(int);
+void merge(int x[],int y[],int arr[],int sx,int sy){
+    int lenx=sx;
+    int leny=sy;
     int i,j,k=0;
 
     while (i<lenx && j<leny){
@@ -32,35 +32,37 @@ void merge(int x[],int y[],int arr[]){
     }
 }
 
-void mergeSort(int arr[]){
-    int lenarr =sizeof(arr)/sizeof(int);
-    if (lenarr<2){
-        return ;} 
-    int lenx=lenarr/2;
-    int x[lenx];
-    int y[lenarr-lenx];
-    for (int i=0;i<lenx;i++){
+// void mergeSort(int arr[]){
+//     int lenarr =sizeof(arr)/sizeof(int);
+//     if (lenarr<2){
+//         return ;} 
+//     int lenx=lenarr/2;
+//     int x[lenx];
+//     int y[lenarr-lenx];
+//     for (int i=0;i<lenx;i++){
 
-        x[i]=arr[i];
-    }
-     for (int i=lenx;i<lenarr;i++){
+//         x[i]=arr[i];
+//     }
+//      for (int i=lenx;i<lenarr;i++){
 
-        y[i]=arr[i];
-    }
-    mergeSort(x);
-    mergeSort(y);
-    merge(x,y,arr); 
-}
+//         y[i]=arr[i];
+//     }
+//     mergeSort(x);
+//     mergeSort(y);
+//     merge(x,y,arr); 
+// }
 
 int main(){
 
 
-int x[]={5,4,3,2,1};
-mergeSort(x);
+int arr[]={8,9,77,66,99};
+int x[]={1,4,5};
+int y[]={2,3};
+merge(x,y,arr,3,2);
 
-for (int i=0;i<sizeof(x)/sizeof(int);i++){
+for (int i=0;i<sizeof(arr)/sizeof(int);i++){
 
-    cout<<x[i]<<" ";
+    cout<<arr[i]<<" ";
 }
 
 return 0;
